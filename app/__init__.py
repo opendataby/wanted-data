f9b88a1ceac213cb997b938945fa2ca43449e260
Pavel Tyslacki <pavel.tyslacki@gmail.com>
2017-04-03 22:16:14 +0300

    add ban ips

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import logging
+import os
+import re
 
-from flask import Flask
+from flask import Flask, request
 from flask.ext.markdown import Markdown
 
 from app.db import db, DataSet, Vote
@@ -18,3 +20,10 @@ app.register_blueprint(routes)
 
 app.logger.addHandler(logging.StreamHandler())
 app.logger.setLevel(logging.INFO)
+
+
+@app.before_request
+def before_request():
+    if any(re.match(re.escape(ip).replace('\*', '\d+'), request.remote_addr)
+           for ip in os.environ.get('BAN_IPS', '').split(',')):
+        os.abort(403)
